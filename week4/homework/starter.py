5b930418d56ec9545d4a51d0b0088085d8e299c1
zack <keller.zr@gmail.com>
2023-06-19 16:51:15 +0000

    part of bonus

--- week4/homework/starter.py
+++ week4/homework/starter.py
@@ -2,8 +2,13 @@ import sys
 import pickle
 import pandas as pd
 
+from google.cloud import storage
 
-def run(year, month):
+# For logging / debugging IAM roles
+import logging
+import http.client
+
+def get_output_file(year, month):
     with open('model.bin', 'rb') as f_in:
         dv, model = pickle.load(f_in)
 
@@ -47,8 +52,46 @@ def run(year, month):
 
     mean_duration = y_pred.mean()
     print(f'Mean Predicted Duration: {mean_duration:.2f}')
+    return output_file
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
 if __name__ == '__main__':
     year = int(sys.argv[1])
     month = int(sys.argv[2])
-    run(year, month)
+    BUCKET = 'mlops-zoomcamp-hw4'
+
+    output_file = get_output_file(year, month)
+
+    logging.basicConfig(level=logging.DEBUG)
+    http.client.HTTPConnection.debuglevel=5
+
+    upload_blob(
+        bucket_name=BUCKET,
+        source_file_name=output_file,
+        destination_blob_name=output_file
+    )
+
