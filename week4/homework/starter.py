18000fd19d77f017d6d0015976b37b54fcf754cc
zack <keller.zr@gmail.com>
2023-06-19 15:37:59 +0000

    hw4-6

--- week4/homework/starter.py
+++ week4/homework/starter.py
@@ -1,87 +1,54 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().system('pip freeze | grep scikit-learn')
-
-
-# In[2]:
-
-
+import sys
 import pickle
 import pandas as pd
 
 
-# In[3]:
+def run(year, month):
+    with open('model.bin', 'rb') as f_in:
+        dv, model = pickle.load(f_in)
+
+    categorical = ['PULocationID', 'DOLocationID']
+
+    def read_data(filename):
+        df = pd.read_parquet(filename)
+
+        df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+        df['duration'] = df.duration.dt.total_seconds() / 60
+
+        df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+        df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+
+        return df
+
+    url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet'
+    df = read_data(url)
 
 
-with open('model.bin', 'rb') as f_in:
-    dv, model = pickle.load(f_in)
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
 
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
-# In[4]:
+    df_result = pd.DataFrame({
+        'ride_id': df.ride_id,
+        'y_pred': y_pred
+    })
 
+    output_file = 'hw4-2.parquet'
 
-categorical = ['PULocationID', 'DOLocationID']
+    df_result.to_parquet(
+        output_file,
+        engine='pyarrow',
+        compression=None,
+        index=False
+    )
 
-def read_data(filename):
-    df = pd.read_parquet(filename)
-    
-    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
-    df['duration'] = df.duration.dt.total_seconds() / 60
+    mean_duration = y_pred.mean()
+    print(f'Mean Predicted Duration: {mean_duration:.2f}')
 
-    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
-
-    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
-    return df
-
-
-# In[5]:
-
-
-df = read_data('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet')
-
-
-# In[6]:
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-
-
-# In[8]:
-
-
-y_pred.std()
-
-
-# In[11]:
-
-
-year = 2022
-month = 2
-
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-
-# In[14]:
-
-
-df_result = pd.DataFrame({
-    'ride_id': df.ride_id,
-    'y_pred': y_pred
-})
-
-output_file = 'hw4-2.parquet'
-
-df_result.to_parquet(
-    output_file,
-    engine='pyarrow',
-    compression=None,
-    index=False
-)
-
+if __name__ == '__main__':
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
+    run(year, month)
